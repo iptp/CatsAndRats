77e091c55bd88bf47f3607fedb151036f9d7ab84
Hugo Dzin <humodz@hotmail.com>
2016-07-15 19:14:26 +0900

    moved complete scenario to complete/ created sample scenario copied sprites into images/

---
+++ SampleScenario/MyWorld.java
@@ -0,0 +1,11 @@
+import greenfoot.*;
+
+public class MyWorld extends World
+{
+    public MyWorld()
+    {
+        // Creates an 8 by 8 cells world, with 60 by 60 pixels cells.
+        // The size of the cell image is also 60 by 60 pixels.
+        super(8, 8, 60);
+    }
+}
